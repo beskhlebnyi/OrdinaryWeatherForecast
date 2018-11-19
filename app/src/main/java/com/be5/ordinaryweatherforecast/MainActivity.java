a6d9e9d0e21364702953ce2e98a4f61886e7295d
be5 <beskhlebnyi@gmail.com>
2018-11-19 10:22:32 +0200

    Initial commit

---
+++ app/src/main/java/com/be5/ordinaryweatherforecast/MainActivity.java
@@ -0,0 +1,13 @@
+therForecastpackage com.be5.ordinaryweatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
